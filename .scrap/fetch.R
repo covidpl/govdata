155aa1f971cf912fd872598392f882f55c7c6b71
Miron B. Kursa <m@mbq.me>
2021-04-11 17:49:16 +0200

    Fetch
    First fetch after scrapper migration to use the new,
    ArcGIS-based reporting system.
    
    Note the changes in the reported data done with this
    change.

--- .scrap/fetch.R
+++ .scrap/fetch.R
@@ -1,101 +1,35 @@
-library(httr)
-library(xml2)
-
-url0<-'https://www.gov.pl%s'
-
-n<-sprintf(url0,'/web/koronawirus/pliki-archiwalne-powiaty')
-message("Downloading index from ",n)
-
-GET(n)->s
-xml_attr(A<-xml_find_all(content(s),'//a[@class="file-download"]'),'href')->W
-
-xml_text(xml_find_all(A,'.//span[@class="extension"]'))->f
-gsub('\u200b','',f)->f
-
-dp<-list()
-dp$a<-function(x){
- gsub(
-  '^(..)_(..)_(..)_?powiaty(_GIS)?.csv$',
-  '20\\3\\2\\1',
- x)->xn
- xn[xn==x]<-NA
- xn
-}
-dp$b<-function(x){
- gsub(
-  '^(..)_(..)_20(..)_?powiaty(_GIS)?.csv$',
-  '20\\3\\2\\1',
- x)->xn
- xn[xn==x]<-NA
- xn
-}
-dp$c<-function(x){
- gsub(
-  '^([0123456789]{2})([0123456789]{2})([0123456789]{4})_powiaty(\\(2\\)|_GIS)?.csv$',
-  '\\3\\2\\1',
- x)->xn
- xn[xn==x]<-NA
- xn
-}
-dp$d<-function(x){
- gsub(
-  '^([0123456789]{4})([0123456789]{2})([0123456789]{2})([0123456789]{6})_rap.*csv$',
-  '\\1\\2\\3',
- x)->xn
- xn[xn==x]<-NA
- xn
+if(!file.exists('danehistorycznepowiaty.zip')){
+ stop("Unfortunately, automatic download from gov.pl is impossible
+ since ArcGIS system is used. Fetch the danehistorycznepowiaty.zip 
+ file by yourself and re-reun")
 }
 
-do.call(cbind,lapply(dp,function(p) p(f)))->K
-apply(K,1,function(x) na.omit(x)[1])->fp
+#Unzip
+tempdir()->d
+unzip('danehistorycznepowiaty.zip',exdir=d)
 
 
-lapply(W,function(w){
- n<-sprintf(url0,w)
- message("Downloading ",n)
- Sys.sleep(1)
- GET(n)->z
- read.csv2(textConnection(content(z,as='text',encoding="cp1250")))
-})->Q
+lapply(list.files(d,patt='csv$',full.names=TRUE),function(fn){ 
+	read.csv2(fn)->x
 
-lapply(1:length(Q),function(e){
- Q[[e]]->x
- x$date<-fp[e]
- x
-})->Q
+ #Extract date from the file name
+	x$date<-gsub('^.*(\\d{8})\\d{6}.*$','\\1',fn)
 
-keys<-list(
- new_cases=c("liczba_przypadkow","Liczba.przypadków","Liczba"),
- deaths=c("zgony","Zgony","Wszystkie.przypadki.śmiertelne"),
- teryt=c("id","teryt","TERYT"),
- date="date"
-)
+ #Extract useful and consistently reported data only
+	x[,c("liczba_przypadkow","zgony","teryt","date")]->x
+	names(x)<-c("new_cases","deaths","teryt","date")
 
-unify<-function(x){
- ans<-list()
- for(key in names(keys)){
-  k<-keys[[key]]
-  k[k%in%names(x)]->k
-  ans[[key]]<-x[[k]]
- }
- as.data.frame(ans)
-}
-
-Q<-lapply(Q,function(raw){
- x<-try(unify(raw))
- if(inherits(x,"try-error")){
-  return(NULL)
- }
- x$new_cases[is.na(x$new_cases)]<-0
- x$deaths[is.na(x$deaths)]<-0
  x[x$teryt!="t0000",]->x
+ x$new_cases[is.na(x$new_cases)]<-0
+ #Deaths number is malformed in some files
+ x$deaths<-as.numeric(x$deaths)
+ x$deaths[is.na(x$deaths)]<-0
  x[order(x$teryt),]->x
  x[(x$new_cases>0) | (x$deaths>0),]->x
- x
-})
-
-Q[!sapply(Q,is.null)]->Q
+	x
+})->Q
 
 for(x in Q)
  write.table(x,row.names=FALSE,sprintf('covid_govpl_%s.tsv',head(x$date,1)))
+ 
 
