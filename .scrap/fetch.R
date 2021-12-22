3dd0e21094c3386f3ef85192b7689985fc55f815
Miron B. Kursa <m@mbq.me>
2021-12-23 00:54:52 +0100

    Structured fetch

--- .scrap/fetch.R
+++ .scrap/fetch.R
@@ -1,44 +1,52 @@
-#Download and unzip
-tempfile()->p
-download.file(
- 'https://arcgis.com/sharing/rest/content/items/e16df1fa98c2452783ec10b0aea4b341/data',
- p
-)
-tempdir()->d
-unzip(p,exdir=d)
+case_death_url<-'https://arcgis.com/sharing/rest/content/items/e16df1fa98c2452783ec10b0aea4b341/data'
+vax_url<-'https://arcgis.com/sharing/rest/content/items/b860f2797f7f4da789cb6fccf6bd5bc7/data'
 
 #pseudo-TERYT of a whole country
 t0<-"t0000"
 
-lapply(list.files(d,patt='csv$',full.names=TRUE),function(fn){ 
-	read.csv2(fn)->x
+download_package<-function(url){
+ #Download and unzip
+ tempfile()->p
+ download.file(url,p)
+ tempdir()->d
+ unzip(p,exdir=d)
+ d
+}
 
- #Extract date from the file name
-	x$date<-gsub('^.*(\\d{8})\\d{6}.*$','\\1',fn)
+import_cd<-function(d){
+ lapply(list.files(d,patt='csv$',full.names=TRUE),function(fn){ 
+  read.csv2(fn)->x
 
- #Extract useful and consistently reported data only
-	x[,c("liczba_przypadkow","zgony","teryt","date")]->x
-	names(x)<-c("new_cases","deaths","teryt","date")
+  #Extract date from the file name
+  x$date<-gsub('^.*(\\d{8})\\d{6}.*$','\\1',fn)
 
- x$new_cases[is.na(x$new_cases)]<-0
- #Deaths number is malformed in some files
- x$deaths<-as.numeric(x$deaths)
- x$deaths[is.na(x$deaths)]<-0
+  #Extract useful and consistently reported data only
+  x[,c("liczba_przypadkow","zgony","teryt","date")]->x
+  names(x)<-c("new_cases","deaths","teryt","date")
 
- stopifnot(sum(x$teryt==t0)<=1)
+  x$new_cases[is.na(x$new_cases)]<-0
+  #Deaths number is malformed in some files
+  x$deaths<-as.numeric(x$deaths)
+  x$deaths[is.na(x$deaths)]<-0
 
- #Convert whole-country total to a number with unknown location
- x$deaths[x$teryt==t0]<-x$deaths[x$teryt==t0]-sum(x$deaths[x$teryt!=t0])
- x$new_cases[x$teryt==t0]<-x$new_cases[x$teryt==t0]-sum(x$new_cases[x$teryt!=t0])
- x$teryt[x$teryt==t0]<-NA
+  stopifnot(sum(x$teryt==t0)<=1)
 
- x[order(x$teryt),]->x
- x[(x$new_cases>0) | (x$deaths>0),]->x
+  #Convert whole-country total to a number with unknown location
+  x$deaths[x$teryt==t0]<-x$deaths[x$teryt==t0]-sum(x$deaths[x$teryt!=t0])
+  x$new_cases[x$teryt==t0]<-x$new_cases[x$teryt==t0]-sum(x$new_cases[x$teryt!=t0])
+  x$teryt[x$teryt==t0]<-NA
 
-	x
-})->Q
+  x[order(x$teryt),]->x
+  x[(x$new_cases>0) | (x$deaths>0),]->x
+
+  x
+ })
+}
 
 if(!interactive()){
+ #Download & convert
+ import_cd(download_package(case_death_url))->Q
+ 
  #Drop each day in a Y/M/D directory structure
  for(x in Q){
   d<-head(x$date,1)
