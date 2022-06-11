23a98557d2a000412644857f26b63983d2ff9b51
Miron B. Kursa <m@mbq.me>
2022-06-11 16:57:14 +0200

    Update

--- .scrap/fetch.R
+++ .scrap/fetch.R
@@ -1,5 +1,8 @@
 case_death_url<-'https://arcgis.com/sharing/rest/content/items/e16df1fa98c2452783ec10b0aea4b341/data'
 vax_url<-'https://arcgis.com/sharing/rest/content/items/b860f2797f7f4da789cb6fccf6bd5bc7/data'
+daily_url<-'https://www.arcgis.com/sharing/rest/content/items/6ff45d6b5b224632a672e764e04e8394/data'
+
+scrap_daily<-'.scrap/daily'
 
 #pseudo-TERYT of a whole country
 t0<-"t0000"
@@ -14,6 +17,20 @@ download_package<-function(url){
  sprintf("%s/%s",tempdir(),paste(sample(letters,10),collapse=''))->d
  unzip(p,exdir=d)
  d
+}
+
+download_daily<-function(){
+ key<-"stan_rekordu_na"
+ read.csv2(daily_url,fileEncoding='cp1250')->x
+ stopifnot(key%in%names(x))
+ unique(x[[key]])->d
+ stopifnot(length(d)==1)
+ stopifnot(grepl("^20\\d{2}-\\d{2}-\\d{2}$",d))
+ gsub('-','',d)->d
+ y<-substr(d,1,4)
+ m<-substr(d,5,6)
+ dir.create(sprintf('%s/%s/%s',scrap_daily,y,m),recursive=TRUE,showWarnings=FALSE)
+ saveRDS(x,file=sprintf("%s/%s/%s/govpl_powdaily_%s.RDS",scrap_daily,y,m,d),compress="xz")
 }
 
 rb<-function(x) do.call(rbind,x)
@@ -49,7 +66,38 @@ import_cd<-function(d){
   x[(x$new_cases>0) | (x$deaths>0),]->x
 
   x
- }))
+ }))->full
+
+ list.files(scrap_daily,patt='govpl_powdaily.*.RDS',full=TRUE,recursive=TRUE)->K
+ #Remove dailies already in a bunch file
+ gsub('^.*(\\d{8}).RDS$','\\1',basename(K))->dates
+ K[!(dates%in%full$date)]->K
+
+ #Load & convert
+ rb(lapply(K,function(fn){
+  gsub('^.*(\\d{8}).RDS$','\\1',basename(fn))->date
+  readRDS(fn)->x
+  x$date<-date
+
+  nck<-"liczba_wszystkich_zakazen"
+  if(nck%in%names(x)) x$liczba_przypadkow<-x[[nck]]
+
+  x[,c("liczba_przypadkow","zgony","teryt","date")]->x
+  names(x)<-c("new_cases","deaths","teryt","date")
+
+  stopifnot(sum(x$teryt==t0)<=1)
+
+  #Convert whole-country total to a number with unknown location
+  x$deaths[x$teryt==t0]<-x$deaths[x$teryt==t0]-sum(x$deaths[x$teryt!=t0])
+  x$new_cases[x$teryt==t0]<-x$new_cases[x$teryt==t0]-sum(x$new_cases[x$teryt!=t0])
+  x$teryt[x$teryt==t0]<-NA
+
+  x[order(x$teryt),]->x
+  x[(x$new_cases>0) | (x$deaths>0),]->x
+
+  x
+ }))->daily
+ rbind(full,daily)
 }
 
 import_tst<-function(d){
@@ -129,6 +177,9 @@ drop_file<-function(Q,fn){
 if(!interactive()){
  #Cases and tests download
  download_package(case_death_url)->govpl
+
+ #Download and integrate daily
+ download_daily()
 
  #Cases convert
  import_cd(govpl)->Qa
